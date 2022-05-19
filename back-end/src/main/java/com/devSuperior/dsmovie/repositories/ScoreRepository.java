871fa5bd77e4641c461e96be6a4b2b804b4cdb9b
unknown <edwanestudos@gmail.com>
2022-05-19 08:53:14 -0300

    Save score

---
+++ back-end/src/main/java/com/devSuperior/dsmovie/repositories/ScoreRepository.java
@@ -0,0 +1,12 @@
+package com.devSuperior.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devSuperior.dsmovie.entities.Score;
+import com.devSuperior.dsmovie.entities.ScorePK;
+
+
+public interface ScoreRepository extends JpaRepository<Score, ScorePK> {
+
+	
+}
