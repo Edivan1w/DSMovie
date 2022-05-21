3cd3ce7928cbbdd2f7d1567c7362e0af9aff989b
unknown <edwanestudos@gmail.com>
2022-05-21 07:46:18 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,19 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/request";
 
 function Listing() {
-    return (
 
+    //forma errada: só para saber se está recebendo a requisição.
+    // e a requisição é uma promices é um objeto que encapsula.
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            console.log(response.data);
+        });
+
+
+    return (
         <>
             <Pagination />
 
